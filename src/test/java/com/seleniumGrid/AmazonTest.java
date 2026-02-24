1a8be340a68786e9a894c18794babc212fd35207
NILANANDHINI20 <nilalistenstoyou@gmail.com>
2026-02-24 18:26:00 +0530

    AssignmentDevOps

---
+++ src/test/java/com/seleniumGrid/AmazonTest.java
@@ -0,0 +1,52 @@
+package com.seleniumGrid;
+
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+
+	public WebDriver driver;
+
+	@Parameters({ "browser" })
+	@Test
+	public void setup(String browser) throws Exception {
+		
+		System.out.println("Remote Driver Connectivity is Started!");
+
+		if (browser.equalsIgnoreCase("chrome")) {
+
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+			System.out.println("Chrome Browser Launched");
+
+		} else if (browser.equalsIgnoreCase("firefox")) {
+ 
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"), new FirefoxOptions());
+			System.out.println("Firefox Browser Launched");
+
+		} else if (browser.equalsIgnoreCase("edge")) {
+
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"), new EdgeOptions());
+			System.out.println("Edge Browser Launched");
+
+		}
+        //SCM TEST
+		driver.manage().window().maximize();  		
+		System.out.println("Remote Driver Connectivity is Completed!");
+		Thread.sleep(10000);
+		driver.get("https://www.amazon.in");
+		Thread.sleep(5000);
+		System.out.println("Title is :" + driver.getTitle());
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
+		driver.findElement(By.id("nav-search-submit-button")).click();
+		System.out.println("Title: " + driver.getTitle());
+		Thread.sleep(3000);
+		driver.quit();
+	}
+}
